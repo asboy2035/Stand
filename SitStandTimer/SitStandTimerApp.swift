85ffe99bc1fc08d1cfda093fee076ad3ba238995
ash <as_supp@icloud.com>
2024-12-10 17:10:27 +0100

    Initial Commit

---
+++ SitStandTimer/SitStandTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SitStandTimerApp.swift
+//  SitStandTimer
+//
+//  Created by ash on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SitStandTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SitStandTimerTests/SitStandTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SitStandTimerTests.swift
+//  SitStandTimerTests
+//
+//  Created by ash on 12/10/24.
+//
+
+import Testing
+@testable import SitStandTimer
+
+struct SitStandTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
